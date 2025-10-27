e7caeadb450ed50c25668727337230ca49310ec5
marcowinni <winistoerfermarco@gmail.com>
2025-10-27 12:19:23 +0100

    änderung orderID

--- src/components/MemoryUploader.tsx
+++ src/components/MemoryUploader.tsx
@@ -2120,7 +2120,7 @@ const MemoryUploader = () => {
           throw new Error(`Fehler beim Erstellen der Bestellung via Funktion: ${errorMessage}`);
       }
       
-      const orderId = functionResponse.order.id;
+      const orderId = functionResponse.orderId;
       const orderFolderPath = `order_${orderId}`;
 
       // 3. Lade jetzt alle Dateien in den bestellungs-spezifischen Ordner hoch
